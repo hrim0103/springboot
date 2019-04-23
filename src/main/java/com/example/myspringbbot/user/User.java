b6cf8cb106f13a4b9f0085f033934c5136afc8cb
Dowon Lee <edowon0623@gmail.com>
2019-04-23 15:41:16 +0900

    Upload swagger functions

--- src/main/java/com/example/myspringbbot/user/User.java
+++ src/main/java/com/example/myspringbbot/user/User.java
@@ -2,12 +2,23 @@ package com.example.myspringbbot.user;
 
 import java.util.Date;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(description="All details about the user. ")
 public class User {
 	private Integer id;
+	@Size(min=2, message="이름은 2글자 이상 입력해 주세요.")
+	@ApiModelProperty(notes="Name should have atleast 2 characters")
 	private String name;
+
+	@Past
+	@ApiModelProperty(notes="Birth date should be in the past")
 	private Date birthDate;
 	
 	public User() {
